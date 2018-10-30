22a151e295f4a607f46f3daa1aa561a63410cd7f
lilianalmeida <44161002+lilianalmeida@users.noreply.github.com>
2018-10-30 21:53:14 +0000

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "empresa.h"
+using namespace std;
+
+int main() {
+	cout << "Bases de código \n\n";
+	cout << "Selecione o que pretende fazer: \n"
+			<< "1 - Ver rankings\n" // rankings de atividade mensal de cada elemento do projeto .....
+			<< "2 - Ver salários de todos o pessoal" //lista com salários e valor total
+			<< "3 - Adicionar utilizador/projeto\n"
+			<< "4 - Entrar com utilizador\n"; //----|
+	//ver informações (ranking, tipo de utilizador (e caracteristicas da classe utilizador), projetos, salário, reputaçao.
+	//entrar num projeto (pedindo codigo de acesso) para : ver informaçoes (caracteristicas da classe projeto), fazer novos commits;
+											 //	criar, eliminar ou juntar (merge) branches (conforme tipo de utilizadors e projetos).
+
+}
